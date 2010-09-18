b4eb54268d19f2ac46fe3fce72c9e8b2a20eeb3a
Jesse <jessemcd1@gmail.com>
2010-09-18 14:53:17 -0700

    Extract from address and subject from received email.

---
+++ Themis.Core/Email/EmailAddress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Themis.Email
+{
+    /// <summary>
+    /// A simple representation of an email address
+    /// </summary>
+    public class EmailAddress
+    {
+        public EmailAddress(string address)
+            : this(address, null)
+        { }
+
+        public EmailAddress(string address, string name)
+        {
+            if (String.IsNullOrEmpty(address))
+                throw new ArgumentException("You must specify an email address", "address");
+
+            Address = address;
+            Name = name;
+        }
+
+        public string Address { get; private set; }
+
+        public string Name { get; private set; }
+
+        public override string ToString()
+        {
+            if (String.IsNullOrEmpty(Name))
+                return Address;
+            else
+                return String.Format("\"{0}\" <{1}>", Name.Replace('"', '\''), Address);
+        }
+    }
+}
