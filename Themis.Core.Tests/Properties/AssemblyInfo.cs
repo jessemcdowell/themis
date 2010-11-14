f70f83d4dd2bc8c0e0dab889640298f441c17777
Jesse McDowell <jessemcd1@gmail.com>
2010-11-14 15:36:02 -0800

    Add test project with example email

---
+++ Themis.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Themis.Core.Tests")]
+[assembly: AssemblyDescription("A simple resource scheduling service.")]
+[assembly: AssemblyProduct("Themis.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright © McDowell 2010")]
+[assembly: AssemblyCompany("Jesse McDowell")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b69f25b0-38a7-4cd1-b1f1-38baf2313d05")]
