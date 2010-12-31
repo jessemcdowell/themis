a3361f3143409a3184f12d1384f35504f9b72f00
Jesse McDowell <jessemcd1@gmail.com>
2010-12-31 00:13:13 -0800

    Parsing DateTime values

---
+++ Themis.Core.Tests/Calendar/VCard/DateTimeValueEncodingTests.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Themis.Calendar.VCard
+{
+    [TestFixture]
+    public class DateTimeValueEncodingTests
+    {
+        private const string Name = "N";
+
+        [Test]
+        [ExpectedException(typeof(AssertionException), MatchType=MessageMatch.Contains, ExpectedMessage="DateTime Value")]
+        public void Assert_DateTime_With_Different_Time_Fails()
+        {
+            DateTime d1 = new DateTime(1998, 01, 18, 23, 00, 00, DateTimeKind.Utc);
+            DateTime d2 = new DateTime(1998, 01, 18, 23, 00, 01, DateTimeKind.Utc);
+
+            AssertDateTime(d1, d2);
+        }
+
+        [Test]
+        [ExpectedException(typeof(AssertionException), MatchType=MessageMatch.Contains, ExpectedMessage="DateTime Kind")]
+        public void Assert_DateTime_With_Different_Kinds_Fails()
+        {
+            DateTime d1 = new DateTime(1998, 01, 18, 23, 00, 00, DateTimeKind.Unspecified);
+            DateTime d2 = new DateTime(1998, 01, 18, 23, 00, 00, DateTimeKind.Utc);
+
+            AssertDateTime(d1, d2);            
+        }
+
+        public static void AssertDateTime(DateTime expected, DateTime actual)
+        {
+            // it seems that this statement won't catch differences in kinds
+            Assert.AreEqual(expected, actual, "DateTime Value");
+            Assert.AreEqual(expected.Kind, actual.Kind, "DateTime Kind");
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidVCardFormatException), ExpectedMessage="DateTime with offset specified not supported")]
+        public void Rfc2445_Invalid_Example_1()
+        {
+            const string input = "19980119T230000-0800";
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            sv.GetDateTime();
+        }
+
+        [Test]
+        public void Rfc2445_Local_Example_2()
+        {
+            const string input = "19980118T230000";
+            DateTime expected = new DateTime(1998, 01, 18, 23, 00, 00, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2445_UTC_Example_3()
+        {
+            const string input = "19980119T070000Z";
+            DateTime expected = new DateTime(1998, 01, 19, 07, 00, 00, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        //[Test]
+        //[Ignore("Not supported")]
+        //public void Rfc2445_Local_With_Timezone_Example_4()
+        //{
+        //    const string input = "19980119T020000";
+        //    VCardSimpleValue inputParameter = new VCardSimpleValue("TZID", "US-Eastern");
+        //
+        //    DateTime expected = new DateTime(1998, 01, 19, 02, 00, 00, DateTimeKind.Unspecified);
+        //
+        //    VCardValue v = new VCardValue(Name, input);
+        //    v.Parameters.Add(inputParameter);
+        //
+        //    DateTime actual = v.GetDateTime();
+        //
+        //    AssertDateTime(expected, actual);
+        //}
+
+        [Test]
+        public void Rfc2445_Local_Example_5()
+        {
+            const string input = "19970714T133000";
+            DateTime expected = new DateTime(1997, 07, 14, 13, 30, 00, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2445_UTC_Example_6()
+        {
+            const string input = "19970714T173000Z";
+            DateTime expected = new DateTime(1997, 07, 14, 17, 30, 00, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2425_DateTime_Example_1_With_Dashes()
+        {
+            const string input = "1996-10-22T14:00:00Z";
+            DateTime expected = new DateTime(1996, 10, 22, 14, 00, 00, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2425_DateTime_Example_2_With_Dashes()
+        {
+            const string input = "1996-08-11T12:34:56Z";
+            DateTime expected = new DateTime(1996, 08, 11, 12, 34, 56, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2425_DateTime_Example_3()
+        {
+            const string input = "19960811T123456Z";
+            DateTime expected = new DateTime(1996, 08, 11, 12, 34, 56, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2425_Date_Example_1_With_Dashes()
+        {
+            const string input = "1985-04-12";
+            DateTime expected = new DateTime(1985, 04, 12, 00, 00, 00, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Rfc2425_Date_Example_3()
+        {
+            const string input = "19850412";
+            DateTime expected = new DateTime(1985, 04, 12, 00, 00, 00, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void Outlook_Utc_DateTime()
+        {
+            const string input = "20101114T222754Z";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 54, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        //[Test]
+        //public void Rfc2425_Example_4_Multiple_Values()
+        //{
+        //    const string input = "1996-10-22T14:00:00Z,1996-08-11T12:34:56Z";
+        //    DateTime[] expected = new DateTime[] {
+        //        new DateTime(1996, 10, 22, 14, 00, 00, DateTimeKind.Utc), 
+        //        new DateTime(1996, 08, 11, 12, 34, 56, DateTimeKind.Utc),
+        //    };
+
+        //    VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+
+        //    VCardEntityList<VCardSimpleValue> actual = sv.GetListValues();
+
+        //    Assert.AreEqual(expected.GetLength(0), actual.Count, "Number of items");
+        //    AssertDateTime(expected[0], actual[0].GetDateTime());
+        //    AssertDateTime(expected[1], actual[1].GetDateTime());
+        //}
+
+        [Test]
+        [ExpectedException(typeof(InvalidVCardFormatException), ExpectedMessage="DateTime does not contain a full date")]
+        public void Incomplete_Date_Fails()
+        {
+            const string input = "1998010";
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            sv.GetDateTime();
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidVCardFormatException), ExpectedMessage = "Date value not numeric")]
+        public void DateTime_With_Letters_In_Date_Fails()
+        {
+            const string input = "201A1114T222754Z";
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            sv.GetDateTime();
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidVCardFormatException), ExpectedMessage = "Time value not numeric")]
+        public void DateTime_With_Letters_In_Time_Fails()
+        {
+            const string input = "20101114TA22754Z";
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            sv.GetDateTime();
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidVCardFormatException), ExpectedMessage = "DateTime does has a time marker but not contain a hours and minutes")]
+        public void DateTime_With_Time_Marker_But_No_Time_Fails()
+        {
+            const string input = "20101114T";
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            sv.GetDateTime();
+        }
+
+        [Test]
+        public void DateTime_Hours_Minutes_Local()
+        {
+            const string input = "20101114T2227";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 00, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void DateTime_Hours_Minutes_Utc()
+        {
+            const string input = "20101114T2227Z";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 00, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void DateTime_With_Fraction_Of_Second_Local()
+        {
+            const string input = "20101114T222754123";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 54, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void DateTime_With_Fraction_Of_Second_Utc()
+        {
+            const string input = "20101114T222754123Z";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 54, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void DateTime_With_Fraction_Of_Second_And_Separators_Local()
+        {
+            const string input = "2010-11-14T22:27:54.123";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 54, DateTimeKind.Unspecified);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+
+        [Test]
+        public void DateTime_With_Fraction_Of_Second_And_Separators_Utc()
+        {
+            const string input = "2010-11-14T22:27:54.123Z";
+            DateTime expected = new DateTime(2010, 11, 14, 22, 27, 54, DateTimeKind.Utc);
+
+            VCardSimpleValue sv = new VCardSimpleValue(Name, input);
+            DateTime actual = sv.GetDateTime();
+
+            AssertDateTime(expected, actual);
+        }
+    }
+}
--- Themis.Core/Calendar/VCard/VCardSimpleValue.cs
+++ Themis.Core/Calendar/VCard/VCardSimpleValue.cs
@@ -23,5 +23,90 @@ namespace Themis.Calendar.VCard
         /// The escaped and encoded form of the value.
         /// </summary>
         public string EscapedValue { get; set; }
+
+
+        /// <summary>
+        /// Parses the escaped value as a DateTime
+        /// </summary>
+        /// <returns>The value as a date time with a kind either in UTC (where time zone is specified) or unspecified (for local)</returns>
+        public DateTime GetDateTime()
+        {
+            string inputText = EscapedValue;
+            string text = inputText;
+
+            if (String.IsNullOrEmpty(text))
+                throw new InvalidVCardFormatException("No value to parse", inputText);
+
+
+            // if it contains dashes for the date, remove them
+            if ((text.Length > 4) && (text[4] == '-'))
+                text = text.Substring(0, 4) + text.Substring(5);
+            if ((text.Length > 6) && (text[6] == '-'))
+                text = text.Substring(0, 6) + text.Substring(7);
+
+            // if it contains time separators, remove them
+            if ((text.Length > 11) && (text[11] == ':'))
+                text = text.Substring(0, 11) + text.Substring(12);
+            if ((text.Length > 13) && (text[13] == ':'))
+                text = text.Substring(0, 13) + text.Substring(14);
+            if ((text.Length > 15) && (text[15] == '.'))
+                text = text.Substring(0, 15) + text.Substring(16);
+
+            // make sure thre isn't an offset
+            if (text.Contains("-"))
+                throw new InvalidVCardFormatException("DateTime with offset specified not supported", inputText);
+
+
+            // get the parts of the date
+            if (text.Length < 8)
+                throw new InvalidVCardFormatException("DateTime does not contain a full date", inputText);
+
+            int year;
+            int month;
+            int day;
+            try
+            {
+                year = Int32.Parse(text.Substring(0, 4), System.Globalization.CultureInfo.InvariantCulture);
+                month = Int32.Parse(text.Substring(4, 2), System.Globalization.CultureInfo.InvariantCulture);
+                day = Int32.Parse(text.Substring(6, 2), System.Globalization.CultureInfo.InvariantCulture);
+            }
+            catch (FormatException ex)
+            {
+                throw new InvalidVCardFormatException("Date value not numeric", inputText, ex);
+            }
+
+            // get the time part if it exists
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+            DateTimeKind kind = DateTimeKind.Unspecified;
+
+            if ((text.Length > 8) && (text[8] == 'T'))
+            {
+                if (text.Length < 13)
+                    throw new InvalidVCardFormatException("DateTime does has a time marker but not contain a hours and minutes", inputText);
+
+                try
+                {
+                    hour = Int32.Parse(text.Substring(9, 2), System.Globalization.CultureInfo.InvariantCulture);
+                    minute = Int32.Parse(text.Substring(11, 2), System.Globalization.CultureInfo.InvariantCulture);
+
+                    if (text.Length >= 15)
+                        second = Int32.Parse(text.Substring(13, 2), System.Globalization.CultureInfo.InvariantCulture);
+                }
+                catch (FormatException ex)
+                {
+                    throw new InvalidVCardFormatException("Time value not numeric", inputText, ex);
+                }
+
+                // see if it's specified in UTC
+                if (text.EndsWith("Z", StringComparison.InvariantCultureIgnoreCase))
+                    kind = DateTimeKind.Utc;
+            }
+
+
+            // return the value
+            return new DateTime(year, month, day, hour, minute, second, kind);
+        }
     }
 }
