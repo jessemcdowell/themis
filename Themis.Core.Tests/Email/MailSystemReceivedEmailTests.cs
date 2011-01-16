284430210f3ad152605b315816c6006847909b50
Jesse McDowell <jessemcd1@gmail.com>
2011-01-16 00:51:31 -0800

    Add unit tests for MailSystem Message integration

---
+++ Themis.Core.Tests/Email/MailSystemReceivedEmailTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Themis.Email
+{
+    [TestFixture]
+    public class MailSystemReceivedEmailTests
+    {
+        [Test]
+        public void Wrap_Outlook_Email_1()
+        {
+            var rm = new MailSystemReceivedEmail(ExampleEmails.Messages.GetOutlook1NewEmail());
+
+            Assert.AreEqual("Test Organizer", rm.From.Name, "From.Name");
+            Assert.AreEqual("testorganizer@fake.foo", rm.From.Address, "From.Address");
+
+            Assert.AreEqual("Test Appointment from Outlook", rm.Subject, "Subject");
+        }
+
+    }
+}
---
+++ Themis.Core.Tests/ExampleEmails/Messages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using ActiveUp.Net.Mail;
+
+namespace Themis.ExampleEmails
+{
+    public static class Messages
+    {
+        private const string BasePath = @"ExampleEmails";
+
+        private static Message GetMessageByFileName(string fileName)
+        {
+            string path = Path.Combine(BasePath, fileName);
+
+            return Parser.ParseMessageFromFile(path);
+        }
+
+        public static Message GetOutlook1NewEmail()
+        {
+            return GetMessageByFileName("Outlook-1-New.txt");
+        }
+
+        public static Message GetOutlook1NewWithSimbolsEmail()
+        {
+            return GetMessageByFileName("Outlook-1-New.WithSymbols.txt");
+        }
+
+    }
+}
